151ba05c85cc01738cd8b9bca516364ed74f9c45
facubaratta <fbaratta@fi.uba.ar>
2025-11-29 18:42:34 -0300

    Add handle_boot to __all__ exports in gateway package

--- src/gateway/__init__.py
+++ src/gateway/__init__.py
@@ -4,5 +4,12 @@ from .config import GatewayConfig
 from .queue_manager import QueueManager
 from .message_handlers import MessageHandlers
 from .client_handler import ClientHandler
+from .boot_handler import handle_boot
 
-__all__ = ['GatewayConfig', 'QueueManager', 'MessageHandlers', 'ClientHandler']
+__all__ = [
+    'GatewayConfig',
+    'QueueManager',
+    'MessageHandlers',
+    'ClientHandler',
+    'handle_boot',
+]
